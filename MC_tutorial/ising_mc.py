eb53828de4d0da7208cbdde494a05d0d1c7aa4fd
Bas Nijholt <basnijholt@gmail.com>
2019-08-26 08:55:43 +0200

    style after rebase

--- MC_tutorial/ising_mc.py
+++ MC_tutorial/ising_mc.py
@@ -12,130 +12,147 @@ import random
 import time
 
 ### Input parameters: ###
-T_list = np.linspace(5.0,0.5,10) #temperature list
-L = 4                            #linear size of the lattice
-N_spins = L**2                   #total number of spins
-J = 1                            #coupling parameter
+T_list = np.linspace(5.0, 0.5, 10)  # temperature list
+L = 4  # linear size of the lattice
+N_spins = L ** 2  # total number of spins
+J = 1  # coupling parameter
 
 ### Critical temperature: ###
-Tc = 2.0/np.log(1.0 + np.sqrt(2))*J  #T/J ~ 2.269
+Tc = 2.0 / np.log(1.0 + np.sqrt(2)) * J  # T/J ~ 2.269
 
 ### Monte Carlo parameters: ###
-n_eqSweeps = 0   #number of equilibration sweeps
-n_measSweeps = 20  #number of measurement sweeps
+n_eqSweeps = 0  # number of equilibration sweeps
+n_measSweeps = 20  # number of measurement sweeps
 
 ### Parameters needed to show animation of spin configurations: ###
 animate = True
-bw_cmap = colors.ListedColormap(['black', 'white'])
+bw_cmap = colors.ListedColormap(["black", "white"])
 
 ### Create a directory where measured observables will be stored: ###
-results_dir = 'Data'
-if not(os.path.isdir(results_dir)):
-  os.mkdir(results_dir)
+results_dir = "Data"
+if not (os.path.isdir(results_dir)):
+    os.mkdir(results_dir)
 
 ### Initially, the spins are in a random state (a high-T phase): ###
-spins = np.zeros(N_spins,dtype=np.int)
+spins = np.zeros(N_spins, dtype=np.int)
 for i in range(N_spins):
-  spins[i] = 2*random.randint(0,1) - 1 #either +1 or -1
+    spins[i] = 2 * random.randint(0, 1) - 1  # either +1 or -1
 
 ### Store each spin's four nearest neighbours in a neighbours array (using periodic boundary conditions): ###
-neighbours = np.zeros((N_spins,4),dtype=np.int)
+neighbours = np.zeros((N_spins, 4), dtype=np.int)
 for i in range(N_spins):
-  #neighbour to the right:
-  neighbours[i,0]=i+1
-  if i%L==(L-1):
-    neighbours[i,0]=i+1-L
-  
-  #upwards neighbour:
-  neighbours[i,1]=i+L
-  if i >= (N_spins-L):
-    neighbours[i,1]=i+L-N_spins
+    # neighbour to the right:
+    neighbours[i, 0] = i + 1
+    if i % L == (L - 1):
+        neighbours[i, 0] = i + 1 - L
 
-  # *********************************************************************** #
-  # **********          1a) FILL IN CODE TO CALCULATE           *********** #
-  # **********  THE NEIGHBOUR TO THE LEFT (IN neighbours[i,2])  *********** #
-  # ********** AND THE DOWNWARDS NEIGHBOUR (IN neighbours[i,3]) *********** #
-  # *********************************************************************** #
-#end of for loop
+    # upwards neighbour:
+    neighbours[i, 1] = i + L
+    if i >= (N_spins - L):
+        neighbours[i, 1] = i + L - N_spins
+
+    # *********************************************************************** #
+    # **********          1a) FILL IN CODE TO CALCULATE           *********** #
+    # **********  THE NEIGHBOUR TO THE LEFT (IN neighbours[i,2])  *********** #
+    # ********** AND THE DOWNWARDS NEIGHBOUR (IN neighbours[i,3]) *********** #
+    # *********************************************************************** #
+# end of for loop
 
 ### Function to calculate the total energy ###
 def getEnergy():
-  currEnergy = 0
-  for i in range(N_spins):
-    currEnergy += -J*( spins[i]*spins[neighbours[i,0]] + spins[i]*spins[neighbours[i,1]] )
-  return currEnergy
-#end of getEnergy() function
+    currEnergy = 0
+    for i in range(N_spins):
+        currEnergy += -J * (
+            spins[i] * spins[neighbours[i, 0]] + spins[i] * spins[neighbours[i, 1]]
+        )
+    return currEnergy
+
+
+# end of getEnergy() function
 
 ### Function to calculate the total magnetization ###
 def getMag():
-  return np.sum(spins)
-#end of getMag() function
+    return np.sum(spins)
+
+
+# end of getMag() function
 
 ### Function to perform one Monte Carlo sweep ###
 def sweep():
-  #do one sweep (N_spins single-spin flips):
-  for i in range(N_spins):
-    #randomly choose which spin to consider flipping:
-    site = random.randint(0,N_spins-1)
-    
-    #calculate the change in energy for the proposed move:
-    E_init = getEnergy()
-    spins[site] = -spins[site] #flip the spin before calculating E_final
-    E_final = getEnergy()
-    spins[site] = -spins[site] #flip the spin back since we might not accept the move
-    deltaE = E_final - E_init
-    # *********************************************************************** #
-    # ************       1c) REPLACE THE ABOVE FIVE LINES.        *********** #
-    # ************ FILL IN CODE TO CALCULATE THE CHANGE IN ENERGY *********** #
-    # ************     USING ONLY THE FOUR NEAREST NEIGHBOURS     *********** #
-    # *********************************************************************** #
-  
-    if (deltaE <= 0) or (random.random() < np.exp(-deltaE/T)):  #Metropolis algorithm
-      #flip the spin:
-      spins[site] = -spins[site]
-  #end loop over i
-#end of sweep() function
+    # do one sweep (N_spins single-spin flips):
+    for i in range(N_spins):
+        # randomly choose which spin to consider flipping:
+        site = random.randint(0, N_spins - 1)
+
+        # calculate the change in energy for the proposed move:
+        E_init = getEnergy()
+        spins[site] = -spins[site]  # flip the spin before calculating E_final
+        E_final = getEnergy()
+        spins[site] = -spins[
+            site
+        ]  # flip the spin back since we might not accept the move
+        deltaE = E_final - E_init
+        # *********************************************************************** #
+        # ************       1c) REPLACE THE ABOVE FIVE LINES.        *********** #
+        # ************ FILL IN CODE TO CALCULATE THE CHANGE IN ENERGY *********** #
+        # ************     USING ONLY THE FOUR NEAREST NEIGHBOURS     *********** #
+        # *********************************************************************** #
+
+        if (deltaE <= 0) or (
+            random.random() < np.exp(-deltaE / T)
+        ):  # Metropolis algorithm
+            # flip the spin:
+            spins[site] = -spins[site]
+    # end loop over i
+
+
+# end of sweep() function
 
 #################################################################################
 ########## Loop over all temperatures and perform Monte Carlo updates: ##########
 #################################################################################
-t1 = time.clock() #for timing
+t1 = time.clock()  # for timing
 for T in T_list:
-  print('\nT = %f' %T)
-  
-  #open a file where observables will be recorded:
-  fileName         = '%s/ising2d_L%d_T%.4f.txt' %(results_dir,L,T)
-  file_observables = open(fileName, 'w')
-  
-  #equilibration sweeps:
-  for i in range(n_eqSweeps):
-    sweep()
+    print("\nT = %f" % T)
 
-  #start doing measurements:
-  for i in range(n_measSweeps):
-    sweep()
+    # open a file where observables will be recorded:
+    fileName = "%s/ising2d_L%d_T%.4f.txt" % (results_dir, L, T)
+    file_observables = open(fileName, "w")
 
-    #Write the observables to file:
-    energy = getEnergy()
-    mag    = getMag()
-    file_observables.write('%d \t %.8f \t %.8f \n' %(i, energy, mag))
+    # equilibration sweeps:
+    for i in range(n_eqSweeps):
+        sweep()
 
-    if animate:
-      #Display the current spin configuration:
-      plt.clf()
-      plt.imshow( spins.reshape((L,L)), cmap=bw_cmap, norm=colors.BoundaryNorm([-1,0,1], bw_cmap.N), interpolation='nearest' )
-      plt.xticks([])
-      plt.yticks([])
-      plt.title('%d x %d Ising model, T = %.3f' %(L,L,T))
-      plt.pause(0.01)
-    #end if
+    # start doing measurements:
+    for i in range(n_measSweeps):
+        sweep()
 
-    if (i+1)%1000==0:
-      print('  %d sweeps complete' %(i+1))
-  #end loop over i
+        # Write the observables to file:
+        energy = getEnergy()
+        mag = getMag()
+        file_observables.write("%d \t %.8f \t %.8f \n" % (i, energy, mag))
 
-  file_observables.close()
-#end loop over temperature
+        if animate:
+            # Display the current spin configuration:
+            plt.clf()
+            plt.imshow(
+                spins.reshape((L, L)),
+                cmap=bw_cmap,
+                norm=colors.BoundaryNorm([-1, 0, 1], bw_cmap.N),
+                interpolation="nearest",
+            )
+            plt.xticks([])
+            plt.yticks([])
+            plt.title("%d x %d Ising model, T = %.3f" % (L, L, T))
+            plt.pause(0.01)
+        # end if
+
+        if (i + 1) % 1000 == 0:
+            print("  %d sweeps complete" % (i + 1))
+    # end loop over i
+
+    file_observables.close()
+# end loop over temperature
 
 t2 = time.clock()
-print('Elapsed time: %f seconds' %(t2-t1))
+print("Elapsed time: %f seconds" % (t2 - t1))
--- MC_tutorial/plot_ising.py
+++ MC_tutorial/plot_ising.py
@@ -8,65 +8,65 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ### Input parameters (these should be the same as in ising_mc.py): ###
-T_list = np.linspace(5.0,0.5,10) #temperature list
-L = 4                            #linear size of the lattice
-N_spins = L**2                   #total number of spins
-J = 1                            #coupling parameter
+T_list = np.linspace(5.0, 0.5, 10)  # temperature list
+L = 4  # linear size of the lattice
+N_spins = L ** 2  # total number of spins
+J = 1  # coupling parameter
 
 ### Critical temperature: ###
-Tc = 2.0/np.log(1.0 + np.sqrt(2))*J
+Tc = 2.0 / np.log(1.0 + np.sqrt(2)) * J
 
 ### Observables to plot as a function of temperature: ###
-energy   = np.zeros(len(T_list))
-mag      = np.zeros(len(T_list))
+energy = np.zeros(len(T_list))
+mag = np.zeros(len(T_list))
 specHeat = np.zeros(len(T_list))
-susc     = np.zeros(len(T_list))
+susc = np.zeros(len(T_list))
 
 ### Loop to read in data for each temperature: ###
-for (iT,T) in enumerate(T_list):
-  file = open('Data/ising2d_L%d_T%.4f.txt' %(L,T), 'r')
-  data = np.loadtxt( file )
+for (iT, T) in enumerate(T_list):
+    file = open("Data/ising2d_L%d_T%.4f.txt" % (L, T), "r")
+    data = np.loadtxt(file)
 
-  E   = data[:,1]
-  M   = abs(data[:,2])
+    E = data[:, 1]
+    M = abs(data[:, 2])
 
-  energy[iT] = np.mean(E)
-  mag[iT]    = np.mean(M)
-  
-  # *********************************************************************** #
-  # *********** 2b) FILL IN CODE TO CALCULATE THE SPECIFIC HEAT *********** #
-  # ***********               AND SUSCEPTIBILITY                *********** #
-  # *********************************************************************** #
-  specHeat[iT] = 0
-  susc[iT]     = 0
-#end loop over T
+    energy[iT] = np.mean(E)
+    mag[iT] = np.mean(M)
 
-plt.figure(figsize=(8,6))
+    # *********************************************************************** #
+    # *********** 2b) FILL IN CODE TO CALCULATE THE SPECIFIC HEAT *********** #
+    # ***********               AND SUSCEPTIBILITY                *********** #
+    # *********************************************************************** #
+    specHeat[iT] = 0
+    susc[iT] = 0
+# end loop over T
+
+plt.figure(figsize=(8, 6))
 
 plt.subplot(221)
-plt.axvline(x=Tc, color='k', linestyle='--')
-plt.plot(T_list, energy/(1.0*N_spins), 'o-')
-plt.xlabel('$T$')
-plt.ylabel('$<E>/N$')
+plt.axvline(x=Tc, color="k", linestyle="--")
+plt.plot(T_list, energy / (1.0 * N_spins), "o-")
+plt.xlabel("$T$")
+plt.ylabel("$<E>/N$")
 
 plt.subplot(222)
-plt.axvline(x=Tc, color='k', linestyle='--')
-plt.plot(T_list, mag/(1.0*N_spins), 'o-')
-plt.xlabel('$T$')
-plt.ylabel('$<|M|>/N$')
+plt.axvline(x=Tc, color="k", linestyle="--")
+plt.plot(T_list, mag / (1.0 * N_spins), "o-")
+plt.xlabel("$T$")
+plt.ylabel("$<|M|>/N$")
 
 plt.subplot(223)
-plt.axvline(x=Tc, color='k', linestyle='--')
-plt.plot(T_list, specHeat/(1.0*N_spins), 'o-')
-plt.xlabel('$T$')
-plt.ylabel('$C_V/N$')
+plt.axvline(x=Tc, color="k", linestyle="--")
+plt.plot(T_list, specHeat / (1.0 * N_spins), "o-")
+plt.xlabel("$T$")
+plt.ylabel("$C_V/N$")
 
 plt.subplot(224)
-plt.axvline(x=Tc, color='k', linestyle='--')
-plt.plot(T_list, susc/(1.0*N_spins), 'o-')
-plt.xlabel('$T$')
-plt.ylabel('$\chi/N$')
+plt.axvline(x=Tc, color="k", linestyle="--")
+plt.plot(T_list, susc / (1.0 * N_spins), "o-")
+plt.xlabel("$T$")
+plt.ylabel("$\chi/N$")
 
-plt.suptitle('%d x %d Ising model' %(L,L))
+plt.suptitle("%d x %d Ising model" % (L, L))
 
 plt.show()
